4e0199b7b4052b0a21423ff1e054899f58c9c1fb
Poornima Nayar <poornimakrishnav@gmail.com>
2020-09-30 11:54:03 +0100

    changes

---
+++ Blazor.DF20.WASM/Pages/CodePartialClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.DF20.WASM.Pages
+{
+    public partial class CodePartialClass
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
